44abb7a9a46c1075b182edc54f6826ec3ab578ef
jonathan <112445400+ak9mee@users.noreply.github.com>
2023-05-05 17:29:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sounds = ["iflookscouldkill", "nostylist", "sunset", "stop",];
+const sounds = ["stop", "iflookscouldkill", "nostylist", "sunset", "leaveher"];
 
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
