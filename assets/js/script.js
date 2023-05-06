f558c1e5245244a5b890df951187a2238594ac09
jonathan <112445400+ak9mee@users.noreply.github.com>
2023-05-05 19:38:14 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,6 +187,15 @@ const musicData = [
     artist: "Luh Tyler",
     musicPath: "./sounds/backflippin.mp3",
   },
+  {
+    backgroundImage: "./assets/images/floridawater.jpg",
+    posterUrl: "./assets/images/floridawater.jpg",
+    title: "Florida Water (Ft. Luh Tyler, Ski Mask The Slump God",
+    album: "None",
+    year: 2023,
+    artist: "Danny Towers & DJ Scheme",
+    musicPath: "./sounds/floridawater.mp3",
+  }
 ];
 
 
